f561bb41b97c08f0e9ddd31c7b0cfa8a1337cee1
ashahrior <ashahrior.cse.mbstu@gmail.com>
2022-06-10 10:39:13 +0600

    deletion options enabled

--- todolist/urls.py
+++ todolist/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add', views.addTodoItem, name='add'),
+    path('completed/<todo_id>', views.completedTodo, name='completed'),
+    path('deleteCompleted', views.deleteCompleted, name='deleteCompleted'),
+    path('deleteAll', views.deleteAll, name='deleteAll')
 ]
